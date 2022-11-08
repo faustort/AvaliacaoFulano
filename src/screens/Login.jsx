502aed44e23aadd79da379f0f10b2bf8d214f332
Your Name <you@example.com>
2022-11-08 14:22:34 -0300

    Primeiro Commit

---
+++ src/screens/Login.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import { styles } from "../lib/style";
+export const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  return (
+    <View style={styles.container}>
+      <TextInput
+        label="Email"
+        mode="outlined"
+        value={email}
+        onChangeText={(emailDigitado) => setEmail(emailDigitado)}
+      />
+      <TextInput
+        label="Senha"
+        mode="outlined"
+        value={password}
+        onChangeText={(valor) => setPassword(valor)}
+        secureTextEntry={true}
+      />
+      <Button mode="contained" onPress={() => console.log("Clicou")} />
+    </View>
+  );
+};
